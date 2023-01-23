1f7697874c01cb7c4b0d784b234cfea7fa08b61e
Dmytro Sushchenko <34779566+OBess@users.noreply.github.com>
2023-01-23 12:41:38 +0200

    Create backbone of app

--- main.cpp
+++ main.cpp
@@ -1,17 +1,44 @@
-#include "clock_timer_app.h"
+//#include "clock_timer_app.h"
 
-#include <QApplication>
+//#include <QApplication>
+
+//int main(int argc, char *argv[])
+//{
+//    QApplication a(argc, argv);
+//    ClockTimerApp w;
+
+//#ifdef Q_OS_ANDROID
+//    // This connect exists to call save function
+//    QObject::connect(&a, &QGuiApplication::applicationStateChanged, &w, &ClockTimerApp::onAppStateChange);
+//#endif // Q_OS_ANDROID
+
+//    w.show();
+//    return a.exec();
+//}
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+//#include "clock_timer_app.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    ClockTimerApp w;
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+    QGuiApplication app(argc, argv);
 
-#ifdef Q_OS_ANDROID
-    // This connect exists to call save function
-    QObject::connect(&a, &QGuiApplication::applicationStateChanged, &w, &ClockTimerApp::onAppStateChange);
-#endif // Q_OS_ANDROID
+    // Registers classes types
 
-    w.show();
-    return a.exec();
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+         &app, [url](QObject *obj, const QUrl &objUrl) {
+             if (!obj && url == objUrl)
+                 QCoreApplication::exit(-1);
+        }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
 }
---
+++ resources/main.qml
@@ -0,0 +1,9 @@
+import QtQuick 2.15
+import QtQuick.Window 2.15
+
+Window {
+    title: "ClockTimerApp"
+    width: 640
+    height: 480
+    visible: true
+}
